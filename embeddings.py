318bd14c1befb399eebce8cc3732626d7ed066a7
Lingeshwaran <lingeshwaran.kv2022cse@sece.ac.in>
2026-05-28 01:05:46 +0530

    updated embeddings.py

--- embeddings.py
+++ embeddings.py
@@ -1,9 +1,15 @@
+from functools import lru_cache
+
 from sentence_transformers import SentenceTransformer
 
-model = SentenceTransformer(
-    "all-MiniLM-L6-v2"
-)
+
+@lru_cache(maxsize=1)
+def get_model():
+
+    return SentenceTransformer(
+        "all-MiniLM-L6-v2"
+    )
 
 def generate_embedding(text):
 
-    return model.encode(text).tolist()
+    return get_model().encode(text).tolist()
